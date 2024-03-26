4be56623d5e7c2d1d85b235510083fb2a2c6a740
Diego Henrique Rodrigues <diego_henriquerodrigues@hotmail.com>
2024-03-26 19:41:29 -0300

    feat: add spring

---
+++ src/test/java/com/dbserver/desafiovotacaoapi/DesafioVotacaoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbserver.desafiovotacaoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioVotacaoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
