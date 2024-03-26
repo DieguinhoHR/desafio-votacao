4f430baa70c7a0f778d3190b41a2df595b66e5b7
Diego Henrique Rodrigues <diego_henriquerodrigues@hotmail.com>
2024-03-26 20:15:51 -0300

    feat: add model de associado para trabalhar com DDD

--- src/main/java/com/dbserver/desafiovotacao/domain/model/Associado.java
+++ src/main/java/com/dbserver/desafiovotacao/domain/model/Associado.java
@@ -1,4 +1,27 @@
 package com.dbserver.desafiovotacao.domain.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
 public class Associado {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(max = 100)
+    private String nome;
 }
