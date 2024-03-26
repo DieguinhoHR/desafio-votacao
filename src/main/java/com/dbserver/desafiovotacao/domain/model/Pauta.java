1a121f57c1537f27035dc1cb0433105ffd2a5bdd
Diego Henrique Rodrigues <diego_henriquerodrigues@hotmail.com>
2024-03-26 20:56:26 -0300

    feat: add atributos na model de pauta

--- src/main/java/com/dbserver/desafiovotacao/domain/model/Pauta.java
+++ src/main/java/com/dbserver/desafiovotacao/domain/model/Pauta.java
@@ -1,4 +1,31 @@
 package com.dbserver.desafiovotacao.domain.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.OffsetDateTime;
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
 public class Pauta {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank
+    @Size(max = 150)
+    private String nome;
+    private OffsetDateTime dataInicio;
+    private String descricao;
 }
